06bdbe54f8cac9c8fd8595d97e3aa354d00c89f0
Víctor Torres <114647477+VictorTorresOrtiz@users.noreply.github.com>
2022-11-01 13:48:56 +0100

    Create 015reescribeIfElseTernario.js

---
+++ JS/015reescribeIfElseTernario.js
@@ -0,0 +1,9 @@
+
+"use strict";
+
+let message;
+
+login == 'Alumno' ? message = 'Hola estudiante, tiene 10 tareas pendientes' :
+login == 'Profesor' ? message = 'Buenos días, tiene que calificar 1000 actividades' :
+login == '' ? message = 'Sin sesión, estudie algo' : 
+message = ''; 
