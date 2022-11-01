921a9d9b20440f4b44ca093756a3a47586f7de54
Víctor Torres <114647477+VictorTorresOrtiz@users.noreply.github.com>
2022-11-01 13:33:38 +0100

    Update 010comparaciones.js

--- JS/010comparaciones.js
+++ JS/010comparaciones.js
@@ -1,8 +1,10 @@
-" use strick"
-5 > 4
-"apple" > "microsoft"
-"2" > "12"
-undefined == null
+
+
+"use strick"
+5 > 4; //Resultado seria TRUE por que el valor es mayor que cuatro
+"apple" > "microsoft";//Apple por que compara el primer caracter de cada string y a > m
+"2" > "12";// 2 por que se esta comparando como un caracter es decir 2 > 1
+alert(undefined == null);//Monstrar y convertir numero
 undefined === null
 null == "\n0\n"
 null === +"\n0\n"
