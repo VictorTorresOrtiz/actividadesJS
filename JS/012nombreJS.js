a53f671d760dfffb06151d93aa16208bb4c37da5
Víctor Torres <114647477+VictorTorresOrtiz@users.noreply.github.com>
2022-11-01 13:40:27 +0100

    Create 012nombreJS.js

---
+++ JS/012nombreJS.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let jsName = "ECMAScript";
+let readName = prompt('¿Cuál es el nombre "oficial" de JavaScript?');
+
+if (jsName == readName) {
+    alert("¡Correcto!"); //Si es igual es correcto
+} else {
+    alert("¿No lo sabes?¡ECMAScript!");
+}
